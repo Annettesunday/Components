b9409f4c84f16c8edb9835183ee599826b1ef70a
annettesunday <annette.odhiambo@andela.com>
2019-07-05 14:24:54 +0300

    ch(components): specify images in jsx uding faker

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
             Sam
           </a>
           <div className="metadata">
-              <span className="date"> Today at 6:00pm</span>
+            <span className="date"> Today at 6:00pm</span>
           </div>
           <div className="text">Nice blog post!</div>
         </div>
