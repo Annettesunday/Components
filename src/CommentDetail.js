1ed08c71cc004d006fc997f218a2ca7509c13656
annettesunday <annette.odhiambo@andela.com>
2019-07-05 15:19:39 +0300

    ch(components): passing multiple props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from "react";
-import faker from "faker";
 
 const CommentDetail = props => {
   console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
           {props.author}
         </a>
-      </div>
-      <div className="metadata">
-        <span className="date"> Today at 6:00pm</span>
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
+        </div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45pm"
+        content="Such a nice post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00am"
+        content="More power to you"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 2:30am"
+        content="Good stuff!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
