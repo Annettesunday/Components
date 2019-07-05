b9876dd804f8a920d9077797db41290684ec702c
annettesunday <annette.odhiambo@andela.com>
2019-07-05 14:51:14 +0300

    ch(index): duplicate a single Component

--- src/index.js
+++ src/index.js
@@ -13,10 +13,35 @@ const App = () => {
           <a href="/" className="author">
             Sam
           </a>
-          <div className="metadata">
-            <span className="date"> Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
+        </div>
+        <div className="metadata">
+          <span className="date"> Today at 6:00pm</span>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+        </div>
+        <div className="metadata">
+          <span className="date"> Today at 6:00pm</span>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+        </div>
+        <div className="metadata">
+          <span className="date"> Today at 6:00pm</span>
         </div>
       </div>
     </div>
